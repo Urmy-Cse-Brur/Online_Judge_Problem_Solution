d2d9b304ed06323432503db2ed2a718f77b39ab3
Arifa Haque Urmy <48214456+Urmy-Cse-Brur@users.noreply.github.com>
2019-09-26 13:55:21 +0600

    B

---
+++ Codeforces/codeforces_1199B.cpp
@@ -0,0 +1,34 @@
+ #include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef pair<ll, int> li;
+typedef pair<int, ll> il;
+typedef vector<ii> vii;
+typedef vector<il> vil;
+typedef vector<li> vli;
+
+#define ff first
+#define ss second
+#define pb push_back
+#define mp make_pair
+#define sz size()
+int dx8[] = {0, 0, 1, 1, 1, -1, -1, -1};
+int dy8[] = {1,-1, 1, -1, 0, 0, -1, 1};
+
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen ("in.txt", "r", stdin);
+    freopen ("out.txt", "w", stdout);
+    #endif
+    double H, L, ans;
+    cin>>H>>L;
+    ans=((L*L)-(H*H))/(2*H);
+    cout<<fixed<<setprecision(13)<<ans<<endl;
+    return 0;
+}
