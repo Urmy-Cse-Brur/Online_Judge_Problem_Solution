a61d9d88951e74cd345704a4f1deade47dfc573c
Arifa Haque Urmy <48214456+Urmy-Cse-Brur@users.noreply.github.com>
2019-09-29 12:33:11 +0600

    Div3 A

---
+++ Codeforces/codeforces_1183A.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef pair<ll, int> li;
+typedef pair<int, ll> il;
+typedef vector<ii> vii;
+typedef vector<il> vil;
+typedef vector<li> vli;
+#define PI acos(-1.0)
+#define ff first
+#define ss second
+#define pb push_back
+#define mp make_pair
+#define sz size()
+int dx8[] = {0, 0, 1, 1, 1, -1, -1, -1};
+int dy8[] = {1,-1, 1, -1, 0, 0, -1, 1};
+
+int sum(int n){
+    int sum=0;
+    while(n>0){
+        sum+=(n%10);
+        n/=10;
+    }
+    return sum;
+}
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen ("in.txt", "r", stdin);
+    freopen ("out.txt", "w", stdout);
+    #endif
+    int n, ans;
+    cin>>n;
+    while(sum(n)%4!=0){
+        n++;
+    }
+    cout<<n;
+    return 0;
+}
