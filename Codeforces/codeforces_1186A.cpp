8ae9021d820a9fc8409421802159d22de590a8c4
Arifa Haque Urmy <48214456+Urmy-Cse-Brur@users.noreply.github.com>
2019-09-29 12:30:15 +0600

    A

---
+++ Codeforces/codeforces_1186A.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef pair<ll, int> li;
+typedef pair<int, ll> il;
+typedef vector<ii> vii;
+typedef vector<il> vil;
+typedef vector<li> vli;
+#define PI acos(-1.0)
+#define ff first
+#define ss second
+#define pb push_back
+#define mp make_pair
+#define sz size()
+int dx8[] = {0, 0, 1, 1, 1, -1, -1, -1};
+int dy8[] = {1,-1, 1, -1, 0, 0, -1, 1};
+
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen ("in.txt", "r", stdin);
+    freopen ("out.txt", "w", stdout);
+    #endif
+    int n, m, k;
+    cin>>n>>m>>k;
+    if (m>=n && k>=n)
+        cout<<"Yes"<<endl;
+    else
+    cout<<"No"<<endl;
+    return 0;
+}
