06603faadc06339aff2165f81ce94e70b3d54d6a
Arifa Haque Urmy <48214456+Urmy-Cse-Brur@users.noreply.github.com>
2019-09-30 14:00:18 +0600

    Ugly Number, Number theory

---
+++ Uva/uva_136.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <iterator>
+#include <vector>
+
+using namespace std;
+
+int main ()
+{
+   // freopen ("in.txt", "r", stdin);
+
+
+            printf("The 1500'th ugly number is 859963392.\n");
+
+
+
+    return 0;
+}
