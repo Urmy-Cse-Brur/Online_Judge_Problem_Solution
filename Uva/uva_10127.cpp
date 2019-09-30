4296b1c9a693d37ef0e2c5c825bbe9d471a934e0
Arifa Haque Urmy <48214456+Urmy-Cse-Brur@users.noreply.github.com>
2019-09-30 13:54:02 +0600

    Number Theory

---
+++ Uva/uva_10127.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main ()
+{
+	int n, c;
+	unsigned long long p;
+	while(cin>>n){
+		
+		c=1;
+		p=1%n;
+		
+		while (p!=0){
+			p=(p*10) + 1;
+			p=p%n;
+			c++;
+		}
+		cout << c << endl;
+		
+	}
+	return 0;
+}
